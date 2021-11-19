3ce1e2c3df3dc6e770353e96559779393fd270c2
Rebel Guy <rebelguysminecraft@gmail.com>
2021-11-19 13:10:47 +1000

    Renamed default mod names

--- src/main/java/dev/rebel/chatoverlay/ChatOverlay.java
+++ src/main/java/dev/rebel/chatoverlay/ChatOverlay.java
@@ -1,11 +1,11 @@
-package dev.asbyth.template;
+package dev.rebel.chatoverlay;
 
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "forgetemplate", name = "Forge Template", version = "1.0")
-public class ForgeTemplate {
+@Mod(modid = "chatoverlay", name = "Chat Overlay", version = "1.0")
+public class ChatOverlay {
     @Mod.EventHandler
     public void onFMLInitialization(FMLInitializationEvent event) {
         // $USER = The username of the currently logged in user.
