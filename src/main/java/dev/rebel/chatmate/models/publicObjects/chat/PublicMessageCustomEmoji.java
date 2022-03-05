3547f562d1bdaa73b0092369405305dc772d66b7
Rebel Guy <rebelguysminecraft@gmail.com>
2022-03-05 10:42:19 +1000

    Updated public schema

--- src/main/java/dev/rebel/chatmate/models/publicObjects/chat/PublicMessageCustomEmoji.java
+++ src/main/java/dev/rebel/chatmate/models/publicObjects/chat/PublicMessageCustomEmoji.java
@@ -3,10 +3,13 @@ package dev.rebel.chatmate.models.publicObjects.chat;
 import dev.rebel.chatmate.models.publicObjects.PublicObject;
 import dev.rebel.chatmate.models.publicObjects.emoji.PublicCustomEmoji;
 
+import javax.annotation.Nullable;
+
 public class PublicMessageCustomEmoji extends PublicObject {
   @Override
-  public Integer GetExpectedSchema() { return 1; }
+  public Integer GetExpectedSchema() { return 2; }
 
-  public PublicMessageText textData;
+  public @Nullable PublicMessageText textData;
+  public @Nullable PublicMessageEmoji emojiData;
   public PublicCustomEmoji customEmoji;
 }
