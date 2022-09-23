6df2ddddf63ef8adc2ef889c0cda5c10c01023ff
Rebel Guy <rebelguysminecraft@gmail.com>
2022-09-24 09:17:50 +1000

    CHAT-296 Added scrolling animation (similar implementation to the chat scrolling)

--- src/main/java/dev/rebel/chatmate/gui/Interactive/ScrollingElement.java
+++ src/main/java/dev/rebel/chatmate/gui/Interactive/ScrollingElement.java
@@ -3,6 +3,9 @@ package dev.rebel.chatmate.gui.Interactive;
 import dev.rebel.chatmate.gui.Interactive.Events.IEvent;
 import dev.rebel.chatmate.gui.Interactive.InteractiveScreen.InteractiveContext;
 import dev.rebel.chatmate.gui.Interactive.Layout.RectExtension;
+import dev.rebel.chatmate.gui.StateManagement.Animated;
+import dev.rebel.chatmate.gui.StateManagement.AnimatedDim;
+import dev.rebel.chatmate.gui.StateManagement.AnimatedFloat;
 import dev.rebel.chatmate.gui.models.Dim;
 import dev.rebel.chatmate.gui.models.DimFactory;
 import dev.rebel.chatmate.gui.models.DimPoint;
@@ -17,13 +20,17 @@ import javax.annotation.Nullable;
 import java.awt.*;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Function;
 
 /** Acts as a container for the child relement. The scroll bar is added to *this* element, not the children. If you need to add any padding or similar, you must wrap this into another container first. */
 public class ScrollingElement extends SingleElement {
+  private final static long ANIMATION_DURATION = 300;
+  private final static Function<Float, Float> EASING_FUNCTION = frac -> 1 - (float)Math.pow(1 - frac, 5);
+
   private @Nullable IElement childElement;
   private @Nullable Dim maxHeight;
   private ScrollBarLayout scrollBarLayout;
-  private @Nullable Scrollbar scrollbar;
+  private @Nullable AnimatedDim scrollingPosition;
 
   public ScrollingElement(InteractiveContext context, IElement parent) {
     super(context, parent);
@@ -32,7 +39,7 @@ public class ScrollingElement extends SingleElement {
     this.childElement = null;
     this.maxHeight = null;
     this.scrollBarLayout = ScrollBarLayout.SIDEBAR;
-    this.scrollbar = null;
+    this.scrollingPosition = null;
   }
 
   public ScrollingElement setElement(@Nullable IElement element) {
@@ -53,27 +60,31 @@ public class ScrollingElement extends SingleElement {
     return this;
   }
 
-  private void setScrollbar(Dim contentHeight, Dim maxHeight) {
-    Dim scrollingPosition;
-    if (this.scrollbar == null) {
-      scrollingPosition = ZERO;
-      super.onInvalidateSize();
-    } else {
-      if (this.scrollbar.scrollingPosition.plus(maxHeight).gt(contentHeight)) {
-        scrollingPosition = contentHeight.minus(maxHeight); // max scrolling position
+  private void updateScrollingPosition() {
+    Dim contentHeight = this.childElement.getLastCalculatedSize().getY();
+
+    boolean scrollingEnabled = this.maxHeight != null && contentHeight.gt(this.maxHeight);
+    if (scrollingEnabled) {
+      if (this.scrollingPosition == null) {
+        this.scrollingPosition = new AnimatedDim(ANIMATION_DURATION, ZERO);
+        this.scrollingPosition.setEasing(EASING_FUNCTION);
+        super.onInvalidateSize();
+
       } else {
-        scrollingPosition = this.scrollbar.scrollingPosition;
+        // only modify the scrolling position if it exceeds the maximum
+        Dim maxScrollingPosition = contentHeight.minus(maxHeight);
+        if (this.scrollingPosition.getTarget().gt(maxScrollingPosition)) {
+          this.scrollingPosition.setImmediate(maxScrollingPosition);
+          super.onInvalidateSize();
+        }
+      }
+
+    } else {
+      if (this.scrollingPosition != null) {
+        this.scrollingPosition = null;
+        super.onInvalidateSize();
       }
     }
-
-    this.scrollbar = new Scrollbar(contentHeight, maxHeight, scrollingPosition);
-  }
-
-  private void removeScrollbar() {
-    if (this.scrollbar != null) {
-      super.onInvalidateSize();
-    }
-    this.scrollbar = null;
   }
 
   @Override
@@ -105,24 +116,25 @@ public class ScrollingElement extends SingleElement {
 
   @Override
   public void onMouseScroll(IEvent<MouseEventData.In> e) {
-    if (this.scrollbar == null) {
+    if (this.scrollingPosition == null) {
       return;
     }
 
-    float strength = super.context.keyboardEventService.isHeldDown(Keyboard.KEY_LSHIFT) ? 1 : 8;
+    float strength = super.context.keyboardEventService.isHeldDown(Keyboard.KEY_LSHIFT) ? 1.5f : 12;
 
     ScrollDirection scrollDirection = e.getData().mouseScrollData.scrollDirection;
     Dim delta = gui(strength).times(scrollDirection == ScrollDirection.DOWN ? 1 : -1);
-    Dim scrollingPosition = this.scrollbar.scrollingPosition.plus(delta);
+    Dim scrollingPosition = this.scrollingPosition.getTarget().plus(delta);
 
+    Dim maxScrollingPosition = this.childElement.getLastCalculatedSize().getY().minus(maxHeight);
     if (scrollingPosition.lt(ZERO)) {
       scrollingPosition = ZERO;
-    } else if (scrollingPosition.gt(this.scrollbar.contentHeight.minus(this.scrollbar.maxHeight))) {
-      scrollingPosition = this.scrollbar.contentHeight.minus(this.scrollbar.maxHeight);
+    } else if (scrollingPosition.gt(maxScrollingPosition)) {
+      scrollingPosition = maxScrollingPosition;
     }
 
-    if (!scrollingPosition.equals(this.scrollbar.scrollingPosition)) {
-      this.scrollbar = new Scrollbar(this.scrollbar.contentHeight, this.scrollbar.maxHeight, scrollingPosition);
+    if (!scrollingPosition.equals(this.scrollingPosition.getTarget())) {
+      this.scrollingPosition.set(scrollingPosition);
       super.onInvalidateSize();
     }
 
@@ -140,19 +152,19 @@ public class ScrollingElement extends SingleElement {
       return new DimPoint(ZERO, ZERO);
     }
 
-    DimPoint fullChildSize = this.childElement.calculateSize(maxContentSize);
-    if (this.maxHeight != null && fullChildSize.getY().gt(this.maxHeight)) {
-      this.setScrollbar(fullChildSize.getY(), this.maxHeight);
-      return new DimPoint(fullChildSize.getX(), this.maxHeight);
+    DimPoint childSize = this.childElement.calculateSize(maxContentSize);
+    if (this.maxHeight != null && childSize.getY().gt(this.maxHeight)) {
+      return new DimPoint(childSize.getX(), this.maxHeight);
     } else {
-      this.removeScrollbar();
-      return fullChildSize;
+      return childSize;
     }
   }
 
   @Override
   public void setBox(DimRect box) {
-    if (this.scrollbar == null || this.childElement == null) {
+    this.updateScrollingPosition();
+
+    if (this.scrollingPosition == null || this.childElement == null) {
       super.setBox(box);
     } else {
       super.setBoxUnsafe(box); // this box is only for this element
@@ -161,7 +173,7 @@ public class ScrollingElement extends SingleElement {
       // give the child its full box, but translate it so that only the scrolled portion overlaps with our own content box.
       // this is better than doing a GL transform because we don't need to worry about applying the transform to interactions as well
       DimRect childBox = super.getContentBox()
-          .withTranslation(new DimPoint(ZERO, this.scrollbar.scrollingPosition.times(-1))) // we are moving the box up to scroll down
+          .withTranslation(new DimPoint(ZERO, this.scrollingPosition.get().times(-1))) // we are moving the box up to scroll down
           .withHeight(this.childElement.getLastCalculatedSize().getY());
       this.childElement.setBox(childBox);
     }
